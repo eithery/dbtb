9019adcb23d8ca12a76b5ce45ceaaf1982333da1
eithery <eithery.pro@gmail.com>
2026-01-31 12:33:45 -0500

    Rmoved Ruby code

--- bin/dbtb.cmd
+++
@@ -1,7 +0,0 @@
-::
-:: Eithery, 2021
-:: Command batch file for Windows
-:: Runs DBTB via ruby
-::
-@echo off
-ruby.exe "%~dpn0" %*
--- lib/dbtb/version.rb
+++
@@ -1,7 +0,0 @@
-#
-# Eithery, 2021
-# Contains the application version
-#
-module Dbtb
-  VERSION = '0.1.0'
-end
